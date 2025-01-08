866b5d72a3630eb335630c6e4a8d9db0d2a30eda
Carlos Siqueira <carlos.siq.upskill@gmail.com>
2025-01-08 10:08:46 +0000

    Created class CalculadoraTest and import junit

---
+++ src/test/java/pt/upskill/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package pt.upskill;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void subtração() {
+        Calculadora calc = new Calculadora();
+
+}
