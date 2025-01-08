3c54d7e9eedf91c0870618c5ed6ee675893149f7
José Pinto <jjmppinto@gmail.com>
2025-01-08 10:29:26 +0000

    Cherry-Pick test class from dev

--- src/test/java/pt/upskill/CalculadoraTest.java
+++ src/test/java/pt/upskill/CalculadoraTest.java
@@ -1,15 +1,40 @@
 package pt.upskill;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
 
-    @org.junit.jupiter.api.Test
-    void subtracao() {
+
+    @Test
+    void testePrimoNegativo() {
+
         Calculadora calc = new Calculadora();
-        calc.digit1 = 10;
-        calc.digit2 = 5;
-        double expResult = 5;
-        double actResult = calc.subtracao(calc.digit1, calc.digit2);
-        assertEquals(expResult, actResult, 0.0);
+        assertFalse(calc.ePrimo(-5));  // Números primos não podem ser negativos
+    }
+
+    @Test
+    public void testePrimo0() {
+        Calculadora calc = new Calculadora();
+        assertFalse(calc.ePrimo(0));  // 0 não é número primo
+    }
+
+    @Test
+    void testeePrimo1() {
+        Calculadora calc = new Calculadora();
+        assertFalse(calc.ePrimo(1));  // 1 não é número primo
+    }
+
+    @Test
+    public void testePrimo2() {
+        Calculadora calc = new Calculadora();
+        assertTrue(calc.ePrimo(2));  // 2 é número primo
+    }
+
+    // Teste com número primo 3
+    @Test
+    public void testePrimo3() {
+        Calculadora calc = new Calculadora();
+        assertTrue(calc.ePrimo(3));  // 3 é número primo
     }
 }
