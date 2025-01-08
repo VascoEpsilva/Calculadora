688c8a170519c2cf51aaa87b8fe52a2eb57521a5
Carlos Siqueira <carlos.siq.upskill@gmail.com>
2025-01-08 10:21:48 +0000

    tested and pass method subtracao. -Updated name method subtração to subtracao.

--- src/test/java/pt/upskill/CalculadoraTest.java
+++ src/test/java/pt/upskill/CalculadoraTest.java
@@ -1,11 +1,15 @@
 package pt.upskill;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
 
     @org.junit.jupiter.api.Test
-    void subtração() {
+    void subtracao() {
         Calculadora calc = new Calculadora();
-
+        calc.digit1 = 10;
+        calc.digit2 = 5;
+        double expResult = 5;
+        double actResult = calc.subtracao(calc.digit1, calc.digit2);
+        assertEquals(expResult, actResult, 0.0);
+    }
 }
